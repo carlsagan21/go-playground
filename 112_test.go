4d39a28fe286744d6b6795232e3b23a6ef0069ca
Kim Soo <k.soo@linecorp.com>
2019-10-06 21:41:10 +0900

    112

---
+++ 112_test.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	remain := sum - root.Val
+	if root.Left == nil && root.Right == nil && remain == 0 {
+		return true
+	}
+	return hasPathSum(root.Left, remain) || hasPathSum(root.Right, remain)
+}
